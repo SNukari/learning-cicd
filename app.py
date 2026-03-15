d5488cacb7c7ffe0ec6aab0bef4c9f24a7736f16
SNukari <saulinukari@hotmail.com>
2026-03-15 08:39:15 +0200

    changed risky os line to more seucre subprocess and changed bandit triggers

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import os
-# making bandit trigger for code flaw with insecure line for security check
-os.system("ls -l")
+import subprocess
 
 def get_weather():
-    return "It's always sunny in the terminal"
+    return "It's always sunny in the terminal!"
 
 if __name__ == "__main__":
+    # If you MUST run a shell command, do it safely:
+    # subprocess.run(["ls", "-l"]) 
     print(get_weather())
