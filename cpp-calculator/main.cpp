fdc6f81d8bce7e817bc13f3ad3f341b376579c6f
SNukari <saulinukari@hotmail.com>
2026-03-15 14:03:54 +0200

    added scan for possible api leaks, added different project folder and own scan and test for each project

---
+++ cpp-calculator/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+
+int a = 5;
+int b = 10;
+int sum = a + b;
+
+cout << "sum of a and b is " << sum << endl;
+
+return 0;
+}
--- python-weather-app/app.py
+++ python-weather-app/app.py
@@ -1,5 +1,8 @@
 import subprocess
 
+# test if security checks work and TruffleHog should flag this!
+GCP_API_KEY = "AIzaSyA-fake-key-12345-testing-purposes"
+
 def get_weather():
     return "It's always sunny in the terminal!"
 
